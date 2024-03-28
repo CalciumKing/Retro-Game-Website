88c34cb5225d6659f3e1b801ef347f08b9cccc56
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2024-03-28 09:20:33 -0400

    final commit

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 $(() => {
-    const consoleSlide = $("#consoleSlideshow")
-    const gameSlide = $("#gameSlideShow");
+    $("#toTopButton").fadeOut();
 
-    const percent = $(window).innerWidth() / 3;
+    if(!document.URL.includes("home") && !document.URL.includes("contact")){
+        const consoleSlide = $("#consoleSlideshow")
+        const gameSlide = $("#gameSlideShow");
 
-    $("#prevConsoleButton").click(() => {
-        consoleSlide.scrollLeft(consoleSlide.scrollLeft() - percent);
-    });
-    $("#nextConsoleButton").click(() => {
-        consoleSlide.scrollLeft(consoleSlide.scrollLeft() + percent);
-    });
+        const percent = $(window).innerWidth() / 3;
 
-    $("#prevGameButton").click(() => {
-        gameSlide.scrollLeft(gameSlide.scrollLeft() - percent);
-    });
-    $("#nextGameButton").click(() => {
-        gameSlide.scrollLeft(gameSlide.scrollLeft() + percent);
-    });
+        $("#prevConsoleButton").click(() => {
+            consoleSlide.scrollLeft(consoleSlide.scrollLeft() - percent);
+        });
+        $("#nextConsoleButton").click(() => {
+            consoleSlide.scrollLeft(consoleSlide.scrollLeft() + percent);
+        });
+
+        $("#prevGameButton").click(() => {
+            gameSlide.scrollLeft(gameSlide.scrollLeft() - percent);
+        });
+        $("#nextGameButton").click(() => {
+            gameSlide.scrollLeft(gameSlide.scrollLeft() + percent);
+        });
+    }
 });
+
+$(window).on("scroll", () => {
+	if ($(this).scrollTop() > $(this).height() / 5) {
+		$("#toTopButton").fadeIn();
+	} else {
+		$("#toTopButton").fadeOut();
+	}
+});
+
+function checkForm() {
+    if(document.URL.includes("contact")) {
+        let form = document.forms["contactForm"];
+        let anyBlank = false;
+        for(let i = 0; i < form.length; i++) {
+            if(i == 2) {
+                i += 5; // skips check boxes
+            }
+            if(form[i].value == "" || form[i].value == null) {
+                anyBlank = true;
+            }
+            console.log(form[i].value); // prints all values
+        }
+        let boxFilledIn = false;
+        for(let i = 3; i < 6; i++) {
+            console.log(form[i].value) // prints all values
+            if(form[i].value != "" || form[i].value != null) {
+                boxFilledIn = true;
+                break; // checks if at least one box is filled in
+            }
+        }
+        if(anyBlank && !boxFilledIn) { // checks if all values are filled in and at least one box is filled in
+            alert("All values must be filled in");
+        }
+    }
+}
